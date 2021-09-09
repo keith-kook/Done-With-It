096d4f9f97f7a43c4c5e0a31663cca2a8c0bca7a
kook <whatup.kook@gmail.com>
2021-09-09 16:47:42 +1000

    Add offline function

--- frontend/app/components/Card.js
+++ frontend/app/components/Card.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import {
-  View,
-  StyleSheet,
-  Image,
-  TouchableWithoutFeedback,
-} from 'react-native';
+import React from "react";
+import { View, StyleSheet, TouchableWithoutFeedback } from "react-native";
+import { Image } from "react-native-expo-image-cache";
 
-import Text from './Text';
-import colors from '../config/colors';
+import Text from "./Text";
+import colors from "../config/colors";
 
-function Card({ title, subTitle, imageUrl, onPress }) {
+function Card({ title, subTitle, imageUrl, onPress, thumbnailUrl }) {
   return (
     <TouchableWithoutFeedback onPress={onPress}>
       <View style={styles.card}>
-        <Image style={styles.image} source={{ uri: imageUrl }} />
+        <Image
+          style={styles.image}
+          tint="light"
+          preview={{ uri: thumbnailUrl }}
+          uri={imageUrl}
+        />
         <View style={styles.detailsContainer}>
           <Text style={styles.title} numberOfLines={1}>
             {title}
@@ -32,18 +33,18 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     backgroundColor: colors.white,
     marginBottom: 20,
-    overflow: 'hidden',
+    overflow: "hidden",
   },
   detailsContainer: {
     padding: 20,
   },
   image: {
-    width: '100%',
+    width: "100%",
     height: 200,
   },
   subTitle: {
     color: colors.secondary,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   title: {
     marginBottom: 7,
